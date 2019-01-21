86acb935926ff9547e3f20120cb2277568e37831
Matt Migliore <mmigliore311@gmail.com>
2019-01-21 16:42:53 -0500

    add trips and games to trip index

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,13 @@ class Trip < ApplicationRecord
   belongs_to :game
   belongs_to :user
   has_one :experience
+
+  def self.trips_and_games
+    self.all.map do |trip|
+       {
+        trip: trip,
+        game: trip.game
+      }
+    end
+  end
 end
